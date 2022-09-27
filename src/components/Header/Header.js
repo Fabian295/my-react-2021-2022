9556c5263016a6bfd5c98951fb7e1449b20f1051
Fabian295 <fabiankanhai70@gmail.com>
2022-09-27 06:15:35 +0200

    Initial commit, setup project, add header aand setup nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+import Nav from './Nav/Nav'
+
+function Header() {
+  return (
+    <header>
+     <h1>Company Name</h1> 
+     <Nav />
+    </header>
+  )
+}
+
+export default Header
