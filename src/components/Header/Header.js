6ee34320b9d3c8dd8cfc692bab6cbd1ec785e7f4
Fabian295 <fabiankanhai70@gmail.com>
2022-09-27 08:45:01 +0200

    Styled header, add logo and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import logo from './logo192.png'
 import './header.css'
 import Nav from './Nav/Nav'
 
 function Header() {
   return (
-    <header>
-     <h1>Company Name</h1> 
-     <Nav />
+    <header className='masthead'>
+      <a href="/">
+        <div className="logo-div">
+         <img src={logo} alt="logo" />
+        </div>
+      </a>
+      <h1>Company Name</h1> 
+      <Nav />
     </header>
   )
 }
